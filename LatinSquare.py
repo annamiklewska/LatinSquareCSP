bbce947f72d99d87a09db190b2d2ae612da5a66e
Ania <takietam908@gmail.com>
2019-05-31 15:21:52 +0200

    work in progress, new version of backtracking

--- LatinSquare.py
+++ LatinSquare.py
@@ -1,19 +1,53 @@
 import numpy as np
+import math
 
 
 class LatinSquare:
 
     def __init__(self, size):
         self.size = size
-        self.square = np.zeros(size)
+        self.length = size**2
+        self.squares = np.zeros(size ** 2, int)
+
+    def get_size(self):
+        return self.size
+
+    def get_square(self):
+        return self.squares
 
     def show_square(self):
-        print(self.square)
+        print(self.squares)
 
     def show_square_as_matrix(self):
+        #print("\n\n************************")
+        print("\n\n\nLATIN SQUARE:")
+        for i in range(0, self.length, self.size):
+            print("\n")
+            for j in range(i, i+self.size, 1):
+                print(self.squares[j], end=' ')
 
-
-    def check_field(self, index):
-        row = ((index+1)/self.size)-1
+    def get_field(self, index):
+        row = math.ceil((index+1)/self.size)-1
         col = index % self.size
         return row, col
+
+    def get_row(self, index):
+        return math.ceil((index+1)/self.size)-1
+
+    def get_column(self, index):
+        return index % self.size
+
+    def get_index(self, row, column):
+        return row * self.size + column
+
+    def is_latin_valid(self, index):  # index should be -1 at start
+
+        for i in range(self.length - 1):  # i = next index
+            rowToCompare = self.get_row(i)
+            columnToCompare = self.get_column(i)
+
+            if self.get_row(index) == rowToCompare or self.get_column(index) == columnToCompare:
+                if self.squares[index] == self.squares[self.get_index(rowToCompare, columnToCompare)] and index is not i:
+                    return False
+
+        return True
--- main.py
+++ main.py
@@ -1,6 +1,81 @@
 import LatinSquare as ls
+import numpy as np
+
+found = False
+
+used = set()
+
+occupied = np.array([])
 
 
-square = ls.LatinSquare(3)
-square.show_square()
+def backtracking_wrong(index, square):
+    global found
+    print("\n\n\nCHECK: ", found, " ", index)
+    if found:
+        return
+    if index < 0 or square.is_latin_valid(index):
+        if index == (square.length-1):
+            print("\nDONE!\n")
+            found = True
+        else:
+            for i in range(1, square.size + 1, 1):
+                    square.squares[index + 1] = i
+                    square.show_square_as_matrix()
+                    backtracking_wrong((index + 1), square)
+
+
+
+def backtracking(index, square):
+    # the board is full
+    sq.show_square_as_matrix()
+    if index == square.length:
+        return True
+    # try all possible values 1-n
+    for i in range(1, square.size + 1, 1):
+        temp = square.squares[index]
+        square.squares[index] = i
+        if square.is_latin_valid(index):
+            if backtracking(index + 1, square):
+                return True
+        else:
+            square.squares[index] = temp
+    square.squares[index] = 0  # undo
+
+
+
+def backtracking_with_forward_checking(index, square):
+    global found
+    global used
+    global occupied
+    print("\n\n\nCHECK: ", found, " ", index)
+    if found:
+        return
+    if index < 0 or square.is_latin_valid(index):
+        if index == (square.length-1):
+            print("\nDONE!\n")
+            found = True
+        else:
+            for i in range(1, square.size + 1, 1):
+                for j in range(square.size):
+                    if square.get_row(index + 1) == square.get_row(j) or square.get_column(index + 1) == square.get_column(j):
+                        np.append(occupied, square.squares[index + 1])
+                        #occupied.append(square.squares[index + 1])
+
+                for j in range(occupied.size):
+                    if square.get_row(index+1) == square.get_row(j) or square.get_column(index + 1) == square.get_column(j):
+                        occupied = []
+                square.squares[index + 1] = i
+                square.show_square_as_matrix()
+                backtracking((index + 1), square)
+
+sq = ls.LatinSquare(3)
+#backtracking_new(0, sq)
+#backtracking(0, sq)
+#backtracking_with_forward_checking(-1, sq)
+#sq.show_square_as_matrix()
+#sq.squares = [0, 1, 2, 1, 2, 0, 2, 0, 1]
+#sq.squares = [1,0,0,0,0,0,0,0,0]
+#sq.squares = [3,3,3,3,3,0,0,0,0]
+sq.show_square_as_matrix()
+#print(sq.is_latin_valid(0))
 
